be78551bede5c33b215d0aff7c33fc6770c817e3
Zack Considine <zack_considine@m015354.aw.org>
2022-10-19 12:35:57 -0700

    added notes today

---
+++ classNotes10-19.py
@@ -0,0 +1,64 @@
+'''#print("Hello World!")
+
+answer = input("What is your name?")
+
+print("Hello! " + answer)
+
+# these examples will be in comments
+print(x)
+print(10)
+print("x")
+
+weekday = False
+
+if weekday:
+    print("it is a weekday")
+else:
+    print("no homework weekend whoooo!!!")'''
+
+def add3(num):
+    result = num + 3
+    print("The result is: " + str(result))
+
+def main():
+
+    print("Hello world")
+    add3(5)
+    add3(20)
+    add3(-40)
+
+    x = 4
+    add3(x)
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
