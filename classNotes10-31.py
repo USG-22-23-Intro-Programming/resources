942aed02ab4e9ada170c86050e43efe5c556062f
Zack Considine <zack_considine@m015354.aw.org>
2022-10-31 12:43:19 -0700

    adding class notes

---
+++ classNotes10-31.py
@@ -0,0 +1,31 @@
+def main():
+
+    #recall lists like:
+    fruits = ["apples", "oranges", "pluots", "tomatoes"]
+
+    # dictionaries are lists where each element has a value associated with it
+
+    # each element is a 'key : value'
+    # works like a function ,  key = input ,  value = output
+    fruits2 = {"apple" : 1.50, "banana" : 0.75, "watermelon" : 6.00, "strawberries" : 3.50}
+
+    #add or change things about the dictionary
+    fruits2.update({"kumquat" : 1.50})
+
+    vegetables = {"lettuce" : 2.50,
+                  "cabbage" : 3.25,
+                  "scallions" : 0.99,
+                  "onion" : 1.15,
+                  "russet potato" : 2.50}
+
+    # access the value based on a key
+    print(vegetables.get("cabbage"))
+    print(vegetables)
+    print(vegetables.pop("onion"))
+    print(vegetables)
+
+    
+
+
+if __name__ == "__main__":
+    main()
