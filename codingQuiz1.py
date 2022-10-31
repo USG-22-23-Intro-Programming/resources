b2f667d18e8438b3166fcffb7193446ba91178d2
Zack Considine <zack_considine@m015354.aw.org>
2022-10-31 11:30:49 -0700

    quiz added

---
+++ codingQuiz1.py
@@ -0,0 +1,33 @@
+# Python Coding Quiz #1
+# Name: ___________
+
+# Read the questions below carefully. Each question describes a
+# method that you will need to define with code. In order to demonstrate
+# that your method works and test your code, you will need to 'un-comment'
+# the method call in the main() method
+
+
+# 1. Define a method which takes in one integer as a parameter variable.
+# This method should print a countdown of the integers from the integer
+# itself to the number 1, each print decreasing the value by 1.
+def countdown():
+
+
+# 2. Define a method which asks the user to input a name.
+# This method should determine if the name starts with 'Mr.' or 'Ms.'
+# If it does, the method should print out 'Hello, ' followed by the name
+# If it does not, the method should print out 'What's up?'
+def teacherTalk():
+    
+
+def main():
+
+    #countdown(5)
+    #countdown(12)
+
+    #teacherTalk()
+    #teacherTalk()
+
+
+if __name__ == "__main__":
+    main()
